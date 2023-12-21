0b3ff3d4e10663c76de061d43c9747c1005decf8
LikeKod <i.serebryanoy@yandex.ru>
2023-12-21 22:19:28 +0300

    add_imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
 const sourcemaps = require("gulp-sourcemaps");
 // const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
@@ -21,7 +22,12 @@ const paths = {
   scripts: {
     src: 'src/scripts/**/*.js',
     dest: 'dist/js/'
+  },
+  images: {
+    src: 'src/img/*',
+    dest: 'dist/images'
   }
+
 }
 
 function clean() {
@@ -58,14 +64,23 @@ function scripts() {
   .pipe(gulp.dest(paths.scripts.dest))
 }
 
+function img() {
+  return gulp.src(paths.images.src)
+  .pipe(imagemin({
+    progressive: true,
+  }))
+  .pipe(gulp.dest(paths.images.dest))
+}
+
 function watch() {
   gulp.watch(paths.styles.src, styles)
   gulp.watch(paths.scripts.src, scripts)
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
+exports.img = img
 exports.styles = styles
 exports.scripts = scripts
 exports.watch = watch
